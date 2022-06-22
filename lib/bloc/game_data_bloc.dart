d252871e7724ca7ddb3ddde8c4871dce1d116ac3
matheusmei <71150878+matheusmei@users.noreply.github.com>
2022-06-22 10:26:10 -0300

    bloc function create!

--- lib/bloc/game_data_bloc.dart
+++ lib/bloc/game_data_bloc.dart
@@ -1,14 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bloc/bloc.dart';
-import 'package:bloc_project/models/data_model.dart';
 import 'package:meta/meta.dart';
+
+import 'package:bloc_project/models/data_model.dart';
+import 'package:bloc_project/repositories/games_repository.dart';
 
 part 'game_data_event.dart';
 part 'game_data_state.dart';
 
 class GameDataBloc extends Bloc<GameDataEvent, GameDataState> {
-  GameDataBloc() : super(GameDataInitialState()) {
-    on<GameDataEvent>((event, emit) {
-      // TODO: implement event handler
+  final GamesRepository gamesRepository;
+
+  GameDataBloc(
+    this.gamesRepository,
+  ) : super(
+          GameDataInitialState(),
+        ) {
+    on<GameDataEvent>((event, emit) async {
+      if (event is LoadGameDataEvent) {
+        emit(
+          GameDataLoadingState(),
+        );
+        List<DataModel>? apiResult = await gamesRepository.getGamesData();
+        if (apiResult == null) {
+          emit(
+            GameDataErrorState(),
+          );
+        } else {
+          emit(
+            GameDataLoadedState(
+              apiResult: apiResult,
+            ),
+          );
+        }
+      }
     });
   }
 }
